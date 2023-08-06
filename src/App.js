f201c451d2dd6660c1a796ee85ee45705d87e4d8
Expak12 <69766780+Expak12@users.noreply.github.com>
2023-08-06 02:26:49 -0600

    Implemented get playlist (spotify API)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import { useState, useEffect } from 'react';
 import Header from './components/Header';
+import SearchResult from './components/SearchResult';
+import style from './App.module.css';
+import $ from 'jquery';
 
 function App() {
+  const [songInput, setSongInput] = useState('');
+  const [resultData, setResultData] = useState([]);
+
+  const getSongs = async (input) => {
+    if (input == "") {
+      return alert("Please type something to search");
+    }
+    let url = 'https://spotify23.p.rapidapi.com/search/?q=' + input + '&type=tracks&offset=0&limit=10&numberOfTopResults=5';
+    let options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '21796eba33mshadf49444971767dp1d2d53jsnb5c2aa035d62',
+        'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      console.log(result.tracks.items);
+      setResultData(result.tracks.items);
+      
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div>
-      <Header />
+      <Header songInput={songInput} setChange={setSongInput} clickFunc={getSongs} />
+      <div className={style.boxContainer}>
+        <SearchResult data={resultData}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/SearchResult.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import style from '../App.module.css';
+
+function SearchResult({data}) {
+  let result;
+  if (Array.isArray(data) && data !== []) {
+    result = data.map((songItem) => {
+      return (<>
+      <li className={style.song} key={songItem.data.id}>
+          <div>
+            <h4>{songItem.data.name}</h4>
+            <p className={style.details}>{songItem.data.artists.items[0].profile.name} | {songItem.data.albumOfTrack.name}</p>
+          </div>
+          <button className={style.plus}>+</button>
+      </li>
+      <hr className={style.hr}/>
+      </>)
+    })
+  }
+
+  return (
+    <div className={style.container}>
+      <h2 className={style.containerTitle}>Results</h2>
+      <ul className={style.resultDataList}>
+        {result}
+      </ul>
+    </div>
+  )
+}
+
+export default SearchResult
